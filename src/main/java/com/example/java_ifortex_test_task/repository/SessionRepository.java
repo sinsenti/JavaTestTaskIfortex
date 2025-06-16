d65f4128ba0e7ab211920e1fd3240249f8573fbf
sinsenti <sinsenti73@gmail.com>
2025-06-16 20:34:01 +0300

    result without one method

--- src/main/java/com/example/java_ifortex_test_task/repository/SessionRepository.java
+++ src/main/java/com/example/java_ifortex_test_task/repository/SessionRepository.java
@@ -18,21 +18,14 @@ public interface SessionRepository extends JpaRepository<Session, Long> {
              ORDER BY started_at_utc ASC
              LIMIT 1
             """, nativeQuery = true)
-    Session getFirstDesktopSession(DeviceType deviceType);
-//    SELECT s.id, s.device_type, s.ended_at_utc, s.started_at_utc, s.user_id
-//    FROM sessions s
-//    JOIN users u ON s.user_id = u.id
-//    WHERE u.deleted = FALSE
-//    AND s.ended_at_utc IS NOT NULL
-//    AND s.ended_at_utc < :endDate
-//    ORDER BY s.started_at_utc DESC
+    Session getFirstDesktopSession(@Param("deviceType") DeviceType deviceType);
 
     @Query(value = """
             SELECT s.* FROM sessions s
-            JOIN users u ON s.user_id = u.id
+                JOIN users u ON s.user_id = u.id
             WHERE u.deleted = FALSE
-            AND s.ended_at_utc IS NOT NULL
-            AND s.ended_at_utc < :endDate
+                AND s.ended_at_utc IS NOT NULL
+                AND s.ended_at_utc < :endDate
             ORDER BY s.started_at_utc DESC
             """, nativeQuery = true)
     List<Session> getSessionsFromActiveUsersEndedBefore2025(@Param("endDate") LocalDateTime endDate);
